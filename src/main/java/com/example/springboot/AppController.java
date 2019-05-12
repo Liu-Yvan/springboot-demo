7bb5ab97dbaba6261ef654529067f228315e3705
Liu-Yvan <502297045@qq.com>
2019-05-12 21:09:46 +0800

    add

---
+++ src/main/java/com/example/springboot/AppController.java
@@ -0,0 +1,26 @@
+package com.example.springboot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @author demo
+ */
+@RestController
+public class AppController {
+
+    @Value("${demo.one}")
+    private String one;
+
+    @GetMapping(value = "/one")
+    public String one() {
+        return this.one;
+    }
+
+    @GetMapping(value = "/hello")
+    public String hello() {
+        return "hello";
+    }
+}
---
+++ src/main/java/com/example/springboot/SpringbootApplication.java
@@ -0,0 +1,15 @@
+package com.example.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("com.example.*")
+@SpringBootApplication
+public class SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
+
+}
